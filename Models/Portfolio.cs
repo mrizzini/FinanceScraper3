eee724ef8b80196bac15e5f2f328844070d3e104
Matthew Rizzini <matthewrizzini@gmail.com>
2018-06-05 23:59:30 -0400

    more table formatting

--- Models/Portfolio.cs
+++ Models/Portfolio.cs
@@ -11,7 +11,6 @@ namespace FinanceScraper3.Models
         public int Id { get; set; }
 
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime Date { get; set; }
 
         [DataType(DataType.Currency)]
