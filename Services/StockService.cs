6e268287ee4b13011768921f205bb1313e42efa8
Matthew Rizzini <matthewrizzini@gmail.com>
2018-06-06 23:55:46 -0400

    continued formatting

--- Services/StockService.cs
+++ Services/StockService.cs
@@ -19,27 +19,10 @@ namespace FinanceScraper3.Services
 
         public async Task<Stock[]> GetStocksAsync(int id)
         {
-
-            // var test = _context.Stocks.Find(id);
-
-            // var test = test.ToArrayAsync();
-
-            // test.Find(id);
-
-            // var test = id.ToString();
-            var test = id;
-            
-
-            // return await _context.Stocks
-            // .Where(x=> x.PortId == test).ToArrayAsync();
-
-              return await _context.Stocks
-            .Where(x=> x.Portfolio.Id == test).ToArrayAsync();
-
-            // return await _context.Stocks.ToArrayAsync();
-
-            // return await test;
-            
+            var snapId = id;
+    
+            return await _context.Stocks
+            .Where(x=> x.Portfolio.Id == snapId).ToArrayAsync();
         }
     }
 }
