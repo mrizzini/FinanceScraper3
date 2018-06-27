1098b4df619b315dc49b9fdb72e726df729ff4ff
Matthew Rizzini <matthewrizzini@gmail.com>
2018-06-26 21:05:04 -0400

    formatting

--- Controllers/SnapshotsController.cs
+++ Controllers/SnapshotsController.cs
@@ -25,32 +25,15 @@ namespace FinanceScraper3.Controllers
         // inject usermanager. we can use this to get the current user in the index action
         private readonly UserManager<ApplicationUser> _userManager;
         
-        // private ApplicationDbContext _ctx; // = new ApplicationDbContext();
-
-        // Constructor. By adding IPortfolioSerivce we need to provide an object that matches the interface when we create this controller
-        // Interfaces help decouple the logic of the app
         public SnapshotsController(IPortfolioService portfolioService, UserManager<ApplicationUser> userManager)
         {
             _portfolioService = portfolioService;
             _userManager = userManager;
         }
         
-
-
-        // Index = /Snapshots
         public async Task<IActionResult> Index(string sortOrder)
         {
-            // returns a Task<Portfolio[]>. We may not have the result right away so we use the await keyword so the code waits until the result is ready before continuing
-            // await lets the code pause on an async operation and the pick up where it left off when the database request finishes, and the rest of our app isnt blocked
-            // Now we must update the Index method signature to return Task<IActionResult> and mark as async
-            // Now our Snapshots Controller depends on the IPortfolio Service interface
-            // Now we must declare which concrete class to use for each interface, in Startup.cs
 
-
-            // looks up current user from User property. If it is somehow null, we use Challenge() to force user to login again if info is missing
-            // We now must update IPortfolioService interface since we are passing Application parameter to GetPortfolioSnapshotsAsync
-            // Then change signature of method in PortfolioService
-            // Then update database to add UserId
             var currentUser = await _userManager.GetUserAsync(User);
             if (currentUser == null) return Challenge();
 
@@ -70,8 +53,6 @@ namespace FinanceScraper3.Controllers
 
             return View(model);
         }
-
-
 
         public async Task<IActionResult> TriggerSnapshot(Portfolio newPortfolio)
         {
